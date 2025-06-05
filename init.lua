d9881e6d91b0bf8b06477bc6ade099dda357c8e4
Arthur Huan <Arthur-Huan@users.noreply.github.com>
2025-06-05 21:22:45 +0800

    Finished all features

--- init.lua
+++ init.lua
@@ -1,51 +1,13 @@
+vim.keymap.set('n', '<Insert>', 'i', { noremap = true, silent = true })
+
+
 local timestamp_pattern = "^%d%d:%d%d:%d%d,%d%d%d%s+-->%s+%d%d:%d%d:%d%d,%d%d%d$"
 local single_number_pattern = "^%d+$"
 
+-- Static variable to store where cursor was when command issued
+local cursor_initial_pos
 
-local function JoinLines()
-  -- Go to start of paragraph
-  vim.cmd('normal! {')
-  local start_lnum = vim.fn.line('.')
-
-  -- Go to end of paragraph
-  vim.cmd('normal! }')
-  local end_lnum = vim.fn.line('.')
-
-  -- Collect lines in the paragraph
-  local lines = {}
-  for lnum = start_lnum, end_lnum do
-    table.insert(lines, vim.fn.getline(lnum))
-  end
-
-  if #lines <= 2 then
-    -- Nothing to join, just return
-    return
-  end
-
-  -- First and second lines unchanged
-  local new_lines = { lines[1], lines[2] }
-
-  -- Join third and onward lines after stripping whitespace
-  local to_join = {}
-  for i = 3, #lines do
-    local stripped = lines[i]:gsub('^%s+', ''):gsub('%s+$', '')
-    table.insert(to_join, stripped)
-  end
-
-  if #to_join > 0 then
-    table.insert(new_lines, table.concat(to_join, ' '))
-  end
-
-  -- Replace paragraph in buffer
-  vim.fn.setline(start_lnum, new_lines)
-  -- Delete any extra lines if the new paragraph is shorter
-  if #new_lines < #lines then
-    vim.fn.deletebufline('%', start_lnum + #new_lines, start_lnum + #lines - 1)
-  end
-end
-
-
-local function ClearBlankLines()
+local function ClearBlankLines(start_line, end_line)
   -- Jump to start of paragraph
   vim.cmd('normal! {')
   local start_line = vim.fn.line('.')
@@ -55,7 +17,7 @@ local function ClearBlankLines()
   -- Check that there are at least three lines in paragraph
   -- Should be number, timestamp, and subtitle content
   -- Otherwise, skip
-  -- TODO: Impelement
+  -- TODO: Impelement above comment
 
   -- Delete blank lines in the paragraph
   lnum = start_line
@@ -63,6 +25,19 @@ local function ClearBlankLines()
     vim.fn.deletebufline('%', lnum)
     -- Do not increment lnum, as lines shift up
     end_line = end_line - 1
+  end
+end
+
+
+local function ReplaceEmptySubtitle()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  local timestamp_pattern = "^%d%d:%d%d:%d%d,%d%d%d%s+-->%s+%d%d:%d%d:%d%d,%d%d%d$"
+
+  for i = 1, #lines - 1 do
+    if lines[i]:match(timestamp_pattern) and lines[i + 1]:match("^%s*$") then
+      vim.api.nvim_buf_set_lines(bufnr, i, i + 1, false, { "`" })
+    end
   end
 end
 
@@ -171,8 +146,57 @@ local function DelLastParagraph()
 end
 
 
+local function JoinLines()
+  -- Jump to start and end of paragraph
+  vim.cmd('normal! {')
+  local start_line = vim.fn.line('.')
+  vim.cmd('normal! }')
+  local end_line = vim.fn.line('.')
+  if start_line > end_line then start_line, end_line = end_line, start_line end
+
+  local bufnr = vim.api.nvim_get_current_buf()
+  local ts_line = nil
+
+  -- Find the timestamp line
+  for lnum = start_line, end_line do
+    local line = vim.api.nvim_buf_get_lines(bufnr, lnum - 1, lnum, false)[1]
+    if line and line:match(timestamp_pattern) then
+      ts_line = lnum
+      break
+    end
+  end
+  if not ts_line then
+    return
+  end
+
+  -- Concatenate lines after timestamp in the paragraph, separated by space
+  local concat = {}
+  for lnum = ts_line + 1, end_line do
+    local line = vim.api.nvim_buf_get_lines(bufnr, lnum - 1, lnum, false)[1]
+    if line then
+      local trimmed = line:match("^%s*(.-)%s*$")
+      table.insert(concat, trimmed)
+    end
+  end
+
+  local joined = table.concat(concat, " ")
+  -- Replace the lines after timestamp with the concatenated line
+  if #concat > 0 then
+    vim.api.nvim_buf_set_lines(bufnr, ts_line, end_line, false, { joined })
+  end
+end
+
+
 local function MainLoop()
+  -- Save initial line number
+  local pos = vim.api.nvim_win_get_cursor(0)
+  last_line = pos[1]
+
+  -- Save number of paragraphs to concat
   local n = vim.v.count1
+
+  ReplaceEmptySubtitle()
+
   if n < 2 then
     return
   elseif n == 2 then
@@ -185,6 +209,9 @@ local function MainLoop()
     end
     DelLastParagraph()
   end
+  -- Return cursor to initial position (original paragraph)
+  vim.api.nvim_win_set_cursor(0, {last_line, 0})
+  JoinLines()
 end
 
-vim.keymap.set('n', 'd', MainLoop, { noremap = true, silent = true })
+vim.keymap.set('n', 'c', MainLoop, { noremap = true, silent = true })
